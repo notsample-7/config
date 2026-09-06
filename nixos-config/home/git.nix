3648f994c504268d44818ecdb4400152a72d84a7
sample279 <224307167+sample279@users.noreply.github.com>
2026-09-06 15:18:31 +0530

    chore: per-forge git identity via includeIf

--- nixos-config/home/git.nix
+++ nixos-config/home/git.nix
@@ -7,12 +7,29 @@
     settings = {
       core.editor = "emacsclient";
       user = {
-        name = "sample279";
+        name = "sample-7";
         email = "224307167+sample279@users.noreply.github.com";
       };
       init.defaultBranch = "main";
     };
+
+    extraConfig = {
+      "includeIf \"gitdir:~/Projects/github/\".path"   = "~/.config/git/email-github";
+      "includeIf \"gitdir:~/Projects/codeberg/\".path" = "~/.config/git/email-codeberg";
+    };
   };
+
+  home.file.".config/git/email-github".text = ''
+    [user]
+      name = sample-7
+      email = 224307167+sample279@users.noreply.github.com
+  '';
+
+  home.file.".config/git/email-codeberg".text = ''
+    [user]
+      name = sample-7
+      email = sample7@noreply.codeberg.org
+  '';
 
   # Lazygit configuration (optional)
   programs.lazygit = {
